7ed235d5024d3d7dc7cb69b3e459bf23784c72d1
Conan Torres <conantorreswf@gmail.com>
2023-07-24 16:42:26 -0300

    parametros nos urls - JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Mudando o link ativo da página
+
 const links = document.querySelectorAll(".header-menu a");
 
 function linkAtivo(link) {
@@ -7,3 +9,17 @@ function linkAtivo(link) {
   }
 }
 links.forEach(linkAtivo);
+
+// Ativando itens da página de orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProdutos(parametro) {
+  const item = document.getElementById(parametro);
+  if (item) {
+    item.checked = true;
+  } else {
+    console.log("Item não existente");
+  }
+}
+parametros.forEach(ativarProdutos);
